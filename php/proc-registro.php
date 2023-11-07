d2bd93dd7abbcb2f43831f437ed13f54e29d4e04
lcosta-d <134619076+lcosta-d@users.noreply.github.com>
2023-11-07 16:27:31 -0300

    Update proc-registro.php

--- php/proc-registro.php
+++ php/proc-registro.php
@@ -1,28 +1,38 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "estacio@estacio";
-$dbname = "faculdade";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "estacio@estacio";
+    $dbname = "faculdade";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Conexão com o banco de dados falhou: " . $conn->connect_error);
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $idade = $_POST["idade"];
+    $email = $_POST["email"];
+    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); 
+
+    
+    $sql = "INSERT INTO usuarios (nome, sobrenome, idade, email, senha) VALUES (?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssiss", $nome, $sobrenome, $idade, $email, $senha);
+
+    if ($stmt->execute()) {
+        echo "Registro bem-sucedido. Você pode fazer login agora.";
+    } else {
+        echo "Erro ao registrar: " . $stmt->error;
+    }
+
+    
+    $stmt->close();
+    $conn->close();
 }
-
-$nome = $_POST["nome"];
-$sobrenome = $_POST["sobrenome"];
-$idade = $_POST["idade"];
-$email = $_POST["email"];
-$senha = password_hash($_POST["senha"], PASSWORD_BCRYPT);
-
-$sql = "INSERT INTO usuarios (nome, sobrenome, idade, email, senha) VALUES ('$nome', '$sobrenome', '$idade', '$email', '$senha')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Conta criada com sucesso.";
-} else {
-    echo "Erro ao criar a conta: " . $conn->error;
-}
-
-$conn->close();
 ?>
